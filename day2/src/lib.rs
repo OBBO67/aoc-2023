339e5cf62374b5566fb0be0a1c599165e6f3c959
Owen Brady <owenbrady1994@gmail.com>
2023-12-02 16:56:37 +0000

    impl day 2 part 1

---
+++ day2/src/lib.rs
@@ -0,0 +1,89 @@
+use std::str::FromStr;
+
+#[derive(Default)]
+struct CubeSet {
+    green: u32,
+    blue: u32,
+    red: u32,
+}
+
+struct Game {
+    id: u32,
+    cube_sets: Vec<CubeSet>,
+}
+
+impl Game {
+    fn game_is_possible(&self) -> bool {
+        for cube_set in &self.cube_sets {
+            if cube_set.red > 12 || cube_set.green > 13 || cube_set.blue > 14 {
+                return false;
+            }
+        }
+        true
+    }
+}
+
+impl FromStr for Game {
+    type Err = ();
+
+    /// Create a game from a game string line
+    /// Example: "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (game_id, cube_sets) = s.split_once(':').unwrap();
+        let (_, game_id) = game_id.split_once(' ').unwrap();
+        let id = game_id.parse::<u32>().unwrap();
+
+        let cube_sets: Vec<CubeSet> = cube_sets
+            .split(';')
+            .map(|cube_set| cube_set.parse::<CubeSet>().unwrap())
+            .collect();
+
+        Ok(Game { id, cube_sets })
+    }
+}
+
+impl FromStr for CubeSet {
+    type Err = ();
+
+    /// Create Cubes set from a cube set string line
+    /// Example: "1 red, 2 green, 6 blue"
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut cube_set = CubeSet::default();
+
+        for cube in s.split(',') {
+            let cube = cube.trim();
+            let (count, colour) = cube.split_once(' ').unwrap();
+            let count = count.parse::<u32>().unwrap();
+            match colour {
+                "red" => cube_set.red = count,
+                "blue" => cube_set.blue = count,
+                "green" => cube_set.green = count,
+                _ => (),
+            }
+        }
+
+        Ok(cube_set)
+    }
+}
+
+pub fn part1(input: &str) -> u32 {
+    input
+        .lines()
+        .map(|line| line.parse::<Game>().unwrap())
+        .filter(|game| game.game_is_possible())
+        .map(|game| game.id)
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &'static str = include_str!("../input.txt");
+
+    #[test]
+    fn part1_test() {
+        let result = part1(INPUT);
+        assert_eq!(result, 2169);
+    }
+}
